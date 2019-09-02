362e072097e933d0b2cd13221c3dd8c1264633b7
foggybrains <dustin.keyes@gmail.com>
2019-09-02 14:53:09 -0600

    query success!

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const queries = require('./queries')
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`listening on ${port}!`))
 
 app.get('/', (request, response) => {
-    response.send('the route worked!')
+    queries.listAll().then(students => response.send(students))
 })
